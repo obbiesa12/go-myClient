eb8e783d71bab787131e081dfd24ae1ef855b56c
obbiesa12 <sanusiobbie@gmail.com>
2025-03-04 11:16:10 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"go-myClient/config"
 	"go-myClient/handlers"
+	"go-myClient/repository"
 	"go-myClient/services"
 	"log"
 
@@ -25,15 +26,15 @@ func main() {
 		Addr: cfg.RedisAddress,
 	})
 
-	clientRepo := repository.NewClientRepository(db)
-	clientService := services.NewClientService(clientRepo, rdb)
-	clientHandler := handlers.NewClientHandler(clientService)
+	clientRepo := repository.NewClient(db)
+	clientSvc := services.NewClientService(clientRepo, rdb)
+	clientHdlr := handlers.NewClientHandler(cfg, clientSvc)
 
 	e := echo.New()
-	e.POST("/clients", clientHandler.CreateClient)
-	e.PUT("/clients/:id", clientHandler.UpdateClient)
-	e.DELETE("/clients/:id", clientHandler.DeleteClient)
-	e.GET("/clients/:id", clientHandler.GetClientByID)
+	e.POST("/clients", clientHdlr.Create)
+	e.PUT("/clients/:id", clientHdlr.Update)
+	e.DELETE("/clients/:id", clientHdlr.Delete)
+	e.GET("/clients/:id", clientHdlr.GetByID)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
